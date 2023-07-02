b64db20bdc1217cfd75a4a5dc6e1f47e9d62d226
Yusufola157 <yusufolaseni602@gmail.com>
2023-07-02 20:55:50 +0100

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
         backgroundImage:{
-          "pp": "url('./src/assets/images/blur.jpg')",
+          "pp": "url('https://img.freepik.com/free-photo/liquid-flame-drops-splashing-vibrant-colors-underwater-generated-by-ai_188544-39973.jpg?t=st=1688327412~exp=1688328012~hmac=68f2739816b67ebddf2575c951137ba3fd56bfaa36a6325d266ac049838a0574&w=826')",
         }
     },
   },
